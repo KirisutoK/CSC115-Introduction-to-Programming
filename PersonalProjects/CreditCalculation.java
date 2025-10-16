81b70311bdd79f27e6d6dbfd747fbfb6eef5d6a4
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-10-16 16:33:44 +0000

    added new files

---
+++ PersonalProjects/CreditCalculation.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CreditCalculation {
+    public static void main(String[]args){
+        //Put your credit hours here per semester and then click the play button (>) from your upper right screen.
+        CreditCalculation();
+
+    }
+
+    public static void CreditCalculation(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your total 2025 Fall Credit Hours:");
+        int Fall2025CreditsHrs = scanner.nextInt();
+
+        System.out.println("Enter your total 2026 Spring Credit Hours:");
+        int Spring2026CreditsHrs = scanner.nextInt();
+
+        System.out.println("Enter your total 2026 Fall Credit Hours:");
+        int Fall2026CreditsHrs = scanner.nextInt();
+
+        System.out.println("Enter your total 2027 Spring Credit Hours:");
+        int Spring2027CreditsHrs = scanner.nextInt();
+
+        int TotalCreditHours = Fall2025CreditsHrs + Spring2026CreditsHrs + Fall2026CreditsHrs + Spring2027CreditsHrs;
+
+        if (TotalCreditHours >=60){
+            System.out.println("Eligible for an Assosiciate!");
+        }
+        else{
+            System.out.println("Your Total Credits is " + TotalCreditHours + " , You need more credit hours!");
+        }
+    }
+}

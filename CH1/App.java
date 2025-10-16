12a167a17f6a265cba15fdb5237afc9c4a65e766
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-10-16 16:23:40 +0000

    Add a new file

---
+++ CH1/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Intro to Programming CSC-115");
+        System.out.println("It works right?");
+        System.out.println("Nope it did not!");
+    }
+}

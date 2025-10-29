bcb68cc5ade7add441209b387ff3a66c02fd022b
Christ Aerjil Dampog <52359975+KirisutoK@users.noreply.github.com>
2025-10-29 03:49:32 +0000

    found out about OOP

---
+++ SS: - Object-Oriented Programing/Practice1/File1.java
@@ -0,0 +1,8 @@
+public class File1 {
+    
+    public static void main(String[] args) {
+        File2 file2 = new File2("Christ");
+
+        System.out.println("File2 Name: " + file2.name);
+    }
+}
---
+++ SS: - Object-Oriented Programing/Practice1/File2.java
@@ -0,0 +1,8 @@
+public class File2 {
+
+    String name;
+
+    public File2(String name) {
+        this.name = name;
+    }
+}
